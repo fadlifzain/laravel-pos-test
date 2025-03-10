0f3c548f80b52edca851f6f2df16992c8594b013
Fadli Fauzi Zain <fadlifzain@gmail.com>
2025-03-10 13:50:20 +0700

    remove center from order table

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="bg-white p-6 rounded-lg shadow-md">
     <h2 class="text-xl font-semibold mb-4">Checked-Out Orders</h2>
-    <table class="w-full border-collapse border border-gray-300 text-center">
+    <table class="w-full border-collapse border border-gray-300">
         <thead>
             <tr class="bg-gray-200">
                 <th class="border border-gray-300 p-2">Order ID</th>
@@ -13,7 +13,7 @@
                 <th class="border border-gray-300 p-2">Actions</th>
             </tr>
         </thead>
-        <tbody class="text-center"> <!-- Ensures content inside tbody is centered -->
+        <tbody>
             @foreach($orders as $order)
                 <tr>
                     <td class="border border-gray-300 p-2">{{ $order->id }}</td>
